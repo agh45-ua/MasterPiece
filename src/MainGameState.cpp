d1492348324377b91cd897b43c9fffc0465bbb52
agh45-ua <98736574+agh45-ua@users.noreply.github.com>
2025-10-16 19:22:48 +0200

    todo añadido, falta probarlo y corregir

--- src/MainGameState.cpp
+++ src/MainGameState.cpp
@@ -12,72 +12,118 @@ MainGameState::MainGameState(){
 void MainGameState::init(){
     ground = {0, (float)screenHeight - groundHeight, (float)screenWidth, (float)groundHeight};
     player1 = {100, ground.y - 50, 50, 50};
-    trianglePos = {screenWidth - 150.0f, ground.y - 50};
-    triangleSize = 50.0f;
+    player2 = {screenWidth - 150.0f, ground.y - 50, 50, 50};
+    
 
-    projectile = {{0,0},{0,0}, false};
-    angle = -30.0f * DEG2RAD;
+    projectile_1 = {{0,0},{0,0}, false};
+    angle_1= -30.0f * DEG2RAD;
+    angle_2= -30.0f * DEG2RAD;
+    
+    
+
+    turno ='1';
 }
 
 void MainGameState::handleInput(){
-    /*
-    // Apuntado con W/S
-    if (IsKeyDown(KEY_W)) angle -= angleSpeed * GetFrameTime();
-    if (IsKeyDown(KEY_S)) angle += angleSpeed * GetFrameTime();
+  
+    //si es el turno del jugador 1 vamos guardando lo que hace hasta que presione enter
+    if (turno =='1'){
+        old_player1=player1;
+        old_angle_1=angle_1;
+        // Calcular ángulo entre el jugador1 y el ratón
+        Vector2 mousePos = GetMousePosition();
+        Vector2 start = {player1.x + player1.width, player1.y + player1.height / 2};
+        angle_1= atan2f(mousePos.y - start.y, mousePos.x - start.x);
 
-    // Limitar el ángulo entre -80° y 0° (para no apuntar hacia abajo)
-    if (angle < -80 * DEG2RAD) angle = -80 * DEG2RAD;
-    if (angle > 0) angle = 0;
 
-    // Disparo
-    if (IsKeyPressed(KEY_SPACE) && !projectile.active) {
-        projectile.active = true; //Activamos el proyectil
-        projectile.pos = {player1.x + player1.width, player1.y + player1.height / 2}; //Posicionamos el proyectil en el borde derecho del jugador
-        float speed = 500.0f; //Velocidad inicial
-        projectile.vel = {speed * cosf(angle), speed * sinf(angle)};
+    
+        // Disparo con ESPACIO o clic izquierdo
+        if ((IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON))) {
+
+            
+            projectile_1.pos = start;
+            float speed = 500.0f;
+            projectile_1.vel = {speed * cosf(angle_1), speed * sinf(angle_1)};
+        }
+
+        // Movimiento del jugador
+        if (IsKeyDown(KEY_A) && player1.x > 0) player1.x -= 200 * GetFrameTime();
+        if (IsKeyDown(KEY_D) && player1.x + player1.width < screenWidth) player1.x += 200 * GetFrameTime();
+        
+        //ya no pillamos mas info del jugador 1 al pulsar enter
+        if(IsKeyPressed(KEY_ENTER)){
+            turno='2';
+        }
     }
-    */
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    //si es el turno del jugador 2 vamos guardando lo que hace hasta que presione enter
+    if (turno =='2'){
+        old_player2=player2;
+        old_angle_2=angle_2;
+        // Calcular ángulo entre el jugador2 y el ratón
+        Vector2 mousePos = GetMousePosition();
+        Vector2 start = {player2.x + player2.width, player2.y + player2.height / 2};
+        angle_2= atan2f(mousePos.y - start.y, mousePos.x - start.x);
 
-    // Calcular ángulo entre el jugador y el ratón
-    Vector2 mousePos = GetMousePosition();
-    Vector2 start = {player1.x + player1.width, player1.y + player1.height / 2};
-    angle = atan2f(mousePos.y - start.y, mousePos.x - start.x);
 
-    // Disparo con ESPACIO o clic izquierdo
-    if ((IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) && !projectile.active) {
+    
+        // Disparo con ESPACIO o clic izquierdo
+        if ((IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON))) {
 
-        projectile.active = true;
-        projectile.pos = start;
-        float speed = 500.0f;
-        projectile.vel = {speed * cosf(angle), speed * sinf(angle)};
+            projectile_2.pos = start;
+            float speed = 500.0f;
+            projectile_2.vel = {speed * cosf(angle_2), speed * sinf(angle_2)};
+        }
+
+         // Movimiento del jugador
+        if (IsKeyDown(KEY_A) && player2.x > 0) player2.x -= 200 * GetFrameTime();
+        if (IsKeyDown(KEY_D) && player2.x + player2.width < screenWidth) player2.x += 200 * GetFrameTime();
+
+        //ya no pillamos mas info del jugador 1
+        if(IsKeyPressed(KEY_ENTER)){
+            turno='r';
+        }
     }
 }
 
 void MainGameState::update(float deltaTime){
     // --- LOGICA ---
-    if (projectile.active) { //Actualizamos el proyectil
-        projectile.vel.y += gravity * deltaTime;
-        projectile.pos.x += projectile.vel.x * deltaTime;
-        projectile.pos.y += projectile.vel.y * deltaTime;
+    if (turno=='r') { //Actualizamos los proyectiles cuando se entre en la resolucion del turno
+        projectile_1.active = true;
+        projectile_2.active = true;
 
+        projectile_1.vel.y += gravity_1 * deltaTime;
+        projectile_1.pos.x += projectile_1.vel.x * deltaTime;
+        projectile_1.pos.y += projectile_1.vel.y * deltaTime;
+
+        projectile_2.vel.y += gravity_2 * deltaTime;
+        projectile_2.pos.x += projectile_2.vel.x * deltaTime;
+        projectile_2.pos.y += projectile_2.vel.y * deltaTime;
         // Colisión con el suelo
-        if (projectile.pos.y > ground.y) {
-            projectile.active = false;
+        if (projectile_1.pos.y > ground.y) {
+            projectile_1.active = false;
         }
+         if (projectile_2.pos.y > ground.y) {
+            projectile_2.active = false;
+        }
+        // Colisión con jugadores
 
-        // Colisión con triángulo
-        Rectangle triBounds = {
-            trianglePos.x - triangleSize,
-            trianglePos.y - triangleSize,
-            triangleSize * 2,
-            triangleSize * 2
-        };
+        if (CheckCollisionPointRec(projectile_1.pos, player2) || CheckCollisionPointRec(projectile_2.pos, player1)) {
+            projectile_1.active = false;
+            projectile_2.active = false;
 
-        if (CheckCollisionPointRec(projectile.pos, triBounds)) {
-            projectile.active = false;
-          
             // Cambiar al estado GameOver
             this->state_machine->add_state(make_unique<GameOverState>(), true);
+        }
+        
+        //cuando los projectile acaben(false) siguiente turno y le toca al j1
+        if (projectile_1.active == false && projectile_2.active == false){ 
+            contador_turno++;
+            turno='1';
         }
     }
 }
@@ -88,34 +134,60 @@ void MainGameState::render(){
     
     ClearBackground(RAYWHITE);
 
+    //Turno
+    DrawText("Turno "+contador_turno,screenWidth/2 -15,screenHeight/2,24,PURPLE);
+    DrawText("Turno de "+turno,screenWidth/2 -15,screenHeight/2,24,PURPLE);
     // Suelo
     DrawRectangleRec(ground, DARKGREEN);
 
-    // Jugador
-    DrawRectangleRec(player1, BLUE);
+    // Jugadores para que sean ocultos los movimientos
+    switch (turno)
+    {
+        case '1':
+        DrawRectangleRec(player1, BLUE);
+        DrawRectangleRec(old_player2, RED);
+        break;
 
-    // Triángulo enemigo
-    DrawTriangle(
-        {trianglePos.x, trianglePos.y - triangleSize},
-        {trianglePos.x - triangleSize, trianglePos.y + triangleSize},
-        {trianglePos.x + triangleSize, trianglePos.y + triangleSize},
-        RED
-    );
+        case '2':
+        DrawRectangleRec(old_player1, BLUE);
+        DrawRectangleRec(player2, RED);
+        break;
+
+        case 'r':
+        DrawRectangleRec(player1, BLUE);
+        DrawRectangleRec(player2, RED);
+        break;
+    }
+    
 
     // Flecha de apuntado
-    Vector2 start = {player1.x + player1.width, player1.y + player1.height / 2};
-    Vector2 end = {start.x + arrowLength * cosf(angle), start.y + arrowLength * sinf(angle)};
-    DrawLineEx(start, end, 4, DARKGRAY);
-    DrawTriangle(
-        end,
-        {end.x - 10 * cosf(angle - 0.3f), end.y - 10 * sinf(angle - 0.3f)},
-        {end.x - 10 * cosf(angle + 0.3f), end.y - 10 * sinf(angle + 0.3f)},
-        DARKGRAY
-    );
+    if(turno=='1' || turno=='r'){//flecha j1
+        Vector2 start = {player1.x + player1.width, player1.y + player1.height / 2};
+        Vector2 end = {start.x + arrowLength_1 * cosf(angle_1), start.y + arrowLength_1 * sinf(angle_1)};
+        DrawLineEx(start, end, 4, DARKGRAY);
+        DrawTriangle(
+            end,
+            {end.x - 10 * cosf(angle_1- 0.3f), end.y - 10 * sinf(angle_1- 0.3f)},
+            {end.x - 10 * cosf(angle_1+ 0.3f), end.y - 10 * sinf(angle_1+ 0.3f)},
+            DARKGRAY
+        );
+    }
+     if(turno=='2' || turno=='r'){//flecha j2
+        Vector2 start2 = {player2.x + player2.width, player2.y + player2.height / 2};
+        Vector2 end2 = {start2.x + arrowLength_2 * cosf(angle_2), start2.y + arrowLength_2 * sinf(angle_2)};
+        DrawLineEx(start2, end2, 4, DARKGRAY);
+        DrawTriangle(
+            end2,
+            {end2.x - 10 * cosf(angle_2- 0.3f), end2.y - 10 * sinf(angle_2- 0.3f)},
+            {end2.x - 10 * cosf(angle_2+ 0.3f), end2.y - 10 * sinf(angle_2+ 0.3f)},
+            DARKGRAY
+        );
+    }
 
-    // Proyectil
-    if (projectile.active)
-        DrawCircleV(projectile.pos, 5, BLACK);
+    // Proyectiles cuando sea resolucion
+    if (turno=='r')
+        DrawCircleV(projectile_1.pos, 5, BLACK);
+        DrawCircleV(projectile_2.pos, 5, BLACK);
 
     DrawText("W/S para apuntar", 20, 20, 20, DARKGRAY);
     DrawText("ESPACIO para disparar", 20, 45, 20, DARKGRAY);
--- src/MainGameState.hpp
+++ src/MainGameState.hpp
@@ -33,23 +33,30 @@ class MainGameState : public GameState
         const int screenWidth = 800;
         const int screenHeight = 600;
 
-        // --- Jugador (cuadrado) ---
-        Rectangle player1;
-        
-        // --- Enemigo (triángulo) ---
-        Vector2 trianglePos;
-        float triangleSize = 50;
+        // --- Jugador 1 y 2 (cuadrado) ---
+        Rectangle player1, old_player1;
+        Rectangle player2, old_player2;
 
         // --- Suelo ---
         const int groundHeight = 100;
         Rectangle ground;
 
-        // --- Proyectil ---
-        Projectile projectile = {{0, 0}, {0, 0}, false};
-        const float gravity = 400.0f;
+        // --- Turnos ---
+        char turno;
+        int contador_turno =1;
+
+        // --- Proyectiles ---
+        Projectile projectile_1 = {{0, 0}, {0, 0}, false};
+        const float gravity_1 = 400.0f;
+        Projectile projectile_2 = {{0, 0}, {0, 0}, false};
+        const float gravity_2 = 400.0f;
 
         // --- Apuntado ---
-        float angle = -30.0f * DEG2RAD; // ángulo inicial (en radianes)
-        const float arrowLength = 60.0f;
-        const float angleSpeed = 60.0f * DEG2RAD; // velocidad de rotación en rad/s
+        float angle_1 = -30.0f * DEG2RAD, old_angle_1; // ángulo inicial (en radianes)
+        const float arrowLength_1 = 60.0f;
+        const float angleSpeed_1 = 60.0f * DEG2RAD; // velocidad de rotación en rad/s
+
+        float angle_2 = -30.0f * DEG2RAD, old_angle_2; // ángulo inicial (en radianes)
+        const float arrowLength_2 = 60.0f;
+        const float angleSpeed_2 = 60.0f * DEG2RAD; // velocidad de rotación en rad/s
 };
